9b75a061ed910f339e6943bdfa6c0318b46364f3
Ric Taglietti <rictaglietti@gmail.com>
2021-09-17 14:57:15 -0400

    completed drum kit

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop function if no corresponding key
+  audio.currentTime = 0; // rewind sound to the start
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+// remove playing class when transition ends
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
